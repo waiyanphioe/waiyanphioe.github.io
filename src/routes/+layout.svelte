739802a2f4e4e9eec9448ce8540e4856f017224f
weiyanphyoe <weiyanphyoe.dev@gmail.com>
2023-02-23 02:40:13 +0630

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+	import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Home - Wai Yan Phioe</title>
+</svelte:head>
+
+<div class="container">
+	<slot />
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		height: 100vh;
+		align-items: center;
+		text-align: center;
+	}
+</style>
